5cabf2914002a3e06a6232bdf7f8dfdcfa859f5e
dkaerit <kaerit@hotmail.com>
2023-06-10 17:30:55 +0100

    multiple login

--- frontend/src/store/auth/auth.module.ts
+++ frontend/src/store/auth/auth.module.ts
@@ -5,21 +5,82 @@ const uri:string = "http://localhost:3000"
 
 export default {
     namespathed: true, 
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                                                             //
+    //                                           STATE                                             //
+    //                                                                                             //
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
     state: () => ({ // variables globales de la aplicación
         token: undefined,
     }),
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                                                             //
+    //                                         MUTATIONS                                           //
+    //                                                                                             //
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
     mutations: {
-       getToken: (state:any, token:string) => { state.token = token } 
+
+       /**
+        * Mutación para establecer el token en el estado de la store.
+        * #param state - Estado de la store
+        * #param token - Token de autenticación
+        */
+
+       setToken: (state:any, token:string) => { state.token = token } 
     },
-    actions: {
-        async autenticationAction({commit}:Triggers, usuario:any): Promise<void> {
-            const response = await axios.post(`${uri}/login`, usuario);
-            commit('getToken', response.data.token);
-        },
-    
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                                                             //
+    //                                         ACTIONS                                             //
+    //                                                                                             //
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+    actions: {   
+        /**
+         * Acción para desautenticar al usuario.
+         * #param commit - Función de commit de Vuex
+         */
+
         async dismissAction({commit}:Triggers): Promise<void> {
-            await commit('getToken', undefined);
+            await commit('setToken', undefined);
             location.reload();
-        }
+        },
+
+        /**
+         * Acción de autenticación con email.
+         * #param commit - Función de commit de Vuex
+         * #param loginIdentifier - Email para autenticación
+         */
+
+        async authenticationActionEmail({ commit }: Triggers, loginIdentifier: string): Promise<void> {
+            const response = await axios.post(`${uri}/login`, { email: loginIdentifier });
+            commit('setToken', response.data.token);
+        },
+
+        /**
+         * Acción de autenticación con teléfono.
+         * #param commit - Función de commit de Vuex
+         * #param loginIdentifier - Teléfono para autenticación
+         */
+
+        async authenticationActionTelefono({ commit }: Triggers, loginIdentifier: string): Promise<void> {
+            const response = await axios.post(`${uri}/login`, { telefono: loginIdentifier });
+            commit('setToken', response.data.token);
+        },
+
+        /**
+         * Acción de autenticación con nombre de usuario.
+         * #param commit - Función de commit de Vuex
+         * #param loginIdentifier - Nombre de usuario para autenticación
+         */
+
+        async authenticationActionUsername({ commit }: Triggers, loginIdentifier: string): Promise<void> {
+            const response = await axios.post(`${uri}/login`, { username: loginIdentifier });
+            commit('setToken', response.data.token);
+        },
     },   
 }
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,39 +1,64 @@
-<script setup lang="ts"> 
+<script setup lang="ts">
+import { ref } from 'vue';
 import InputBox from "../components/InputBox.vue";
 import SocialButton from "../components/SocialButton.vue";
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+// Getter y setter para el valor de userEmailOrTlfn en la store
+const loginIdentifier = ref('');
+
+// Mapa de expresiones regulares y nombres de acciones
+const actionsMap = {
+  authenticationActionEmail: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  authenticationActionTelefono: /^\d+$/,
+  authenticationActionUsername: /.*/
+};
+
+/**
+ * Función para manejar el evento de inicio de sesión
+ * #returns void
+ */
+ const login = (): void => {
+  const [action] = Object.entries(actionsMap)
+  .find(([, regex]) => regex.test(loginIdentifier.value)) || ['authenticationActionUsername'];
+
+  store.dispatch(action, { value: loginIdentifier.value });
+};
+
 </script>
 
 <template>
-<div class="box" id="login-container">
-  <!-- Fila donde se ecnuentra el logo -->
-  <div class="row" id="logo-row">
-    <div class="logo-col col-xs-12">
-      <img src="/logo.svg" alt="Logo" class="logo"/>
+  <div class="box" id="login-container">
+    <div class="row" id="logo-row">
+      <div class="logo-col col-xs-12">
+        <img src="/logo.svg" alt="Logo" class="logo"/>
+      </div>
     </div>
-  </div>
 
-  <!-- Fila donde se encuentran los botones del login-->
-  <div class="row" id="login-form">
-    <h1 class="title">Inicia sesión en Werply</h1>
-    <p>Desata tu creatividad y construye narrativas inolvidables junto al resto de personajes</p>
+    <div class="row" id="login-form">
+      <h1 class="title">Inicia sesión en Werply</h1>
+      <p>Desata tu creatividad narrativa y construye historias inolvidables junto a otros personajes.</p>
 
-    <!-- Caja de botones-->
-    <div class="buttons">
-      <SocialButton provider="google" type="primary" />
-      <SocialButton provider="microsoft" type="primary" />
-      <form class="form" id="alternative-login">
-        <InputBox label="Teléfono, email o nombre de usuario" name="username" autocomplete="user" />
-        <button type="submit" class="primary">Siguiente</button>
-        <button class="secondary" id="forgot-password">¿Olvidaste tu contraseña?</button>
-      </form>
-      <div class="box" id="additional-info">
-        <span class="text">¿No tienes una cuenta?</span>
-        <a href="#" class="link">Regístrate</a>
+      <div class="buttons">
+        <SocialButton provider="google" type="primary" />
+        <SocialButton provider="microsoft" type="primary" />
+        <br/>
+        <form class="form" id="alternative-login" @submit.prevent="login">
+          <InputBox label="Teléfono, email o nombre de usuario" name="username" autocomplete="user" v-model="loginIdentifier" />
+          <button type="submit" class="primary">Siguiente</button>
+          <button class="secondary" id="forgot-password">¿Olvidaste tu contraseña?</button>
+        </form>
+        <div class="box" id="additional-info">
+          <span class="text">¿No tienes una cuenta?</span>
+          <a href="#" class="link">Regístrate</a>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
+
 
 <style scoped lang="scss">
 img.logo {
@@ -44,6 +69,7 @@ img.logo {
 .box#login-container {
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 
 .logo-col {
@@ -58,6 +84,8 @@ img.logo {
   margin: 40px 0px 40px 0px;
   flex-direction: column;
   text-align: center;
+  max-width: 600px;
+  align-items: center;
 }
 
 #logo-row {
@@ -78,7 +106,8 @@ form#alternative-login {
 .buttons {
     display: flex;
     flex-direction: column;
-    padding: 15px 10%;
+    padding: 15px 7%;
+    width: fit-content;
 }
 
 </style>
