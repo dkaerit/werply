a905947bf72ed9b1deeef2020b85f941bad54d2e
Diego Vázquez Campos <kaerit@hotmail.com>
2023-11-06 21:54:50 +0000

    dockerized

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 8080,
+  },
   plugins: [
     vue({ template: {transformAssetUrls} }),
     vuetify({ autoImport: true, }),
